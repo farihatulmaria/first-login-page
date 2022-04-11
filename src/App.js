e76d281f5887db1a90f5bcf22ffa56f483970694
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-04-11 17:16:10 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
-import Login from './Components/Login/Login';
 function App() {
   return (
     <>
       <Header/>
       <Routes>
         <Route path='/'/>
-        <Route path='/login' element={<Login/>}/>
+        {/* <Route path='/login' element={<Login/>}/>
+        <Route path='/create-account' element={<SignUp/>}/> */}
       </Routes>
     </>
   );
